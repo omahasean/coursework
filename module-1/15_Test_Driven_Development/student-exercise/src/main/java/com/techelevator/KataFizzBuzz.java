4da11e095797e12dbefd3d1c66d1d9eb96321a8c
Sean Griswold <omahasean@gmail.com>
2020-02-03 20:11:21 -0500

    FizzBuzzIfNumberDivisibleBy3

--- module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -2,4 +2,8 @@ package com.techelevator;
 
 public class KataFizzBuzz {
 
+	public String fizzBuzzSpecial(int n) {
+		return "";
+	}
+
 }
--- module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -4,5 +4,14 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataFizzBuzzTest {
+	
+	KataFizzBuzz fizzBuzz = new KataFizzBuzz();
+	
+	@Test
+	public void FizzBuzzIfNumberIsDivisibleBy3() {
+		Assert.assertEquals("Fizz", fizzBuzz.fizzBuzzSpecial(3));
+		Assert.assertEquals("Fizz", fizzBuzz.fizzBuzzSpecial(21));
+		Assert.assertEquals("Fizz", fizzBuzz.fizzBuzzSpecial(99));
+	}
 
 }
