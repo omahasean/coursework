b028fdabbde65f458524630ede333a7e6138257e
Sean Griswold <omahasean@gmail.com>
2020-02-03 20:21:37 -0500

    made passing tests for TestFizzBuzzIfDivisibleBy3and5

--- module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -3,7 +3,10 @@ package com.techelevator;
 public class KataFizzBuzz {
 
 	public String fizzBuzzSpecial(int n) {
-		if(n%3==0) {
+		if (n%5==0 && n%3==0) {
+			return "FizzBuzz";
+		}
+		else if(n%3==0) {
 			return "Fizz";
 		}
 		else if (n%5==0) {
