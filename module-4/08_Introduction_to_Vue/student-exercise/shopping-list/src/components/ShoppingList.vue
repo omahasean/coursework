5b71520129ef7158d961b4976c43d8ca0fc6c4a3
Sean Griswold <omahasean@gmail.com>
2020-03-29 19:00:21 -0400

    shopping list vue complete

---
+++ module-4/08_Introduction_to_Vue/student-exercise/shopping-list/src/components/ShoppingList.vue
@@ -0,0 +1,50 @@
+<template>
+    <ul class="shopping-list">
+        <h1>My Shopping list</h1>
+        <li>Steak</li>
+        <li class="completed">Potatoes</li>
+        <li>Spinach</li>
+        <li>Carrots</li>
+        <li>Jelly</li>
+        <li>Peanut Butter</li>
+        <li>Bread</li>
+    </ul>
+    
+</template>
+<style>
+.shopping-list {
+    width:450px;
+    background: #fff;
+    margin: 50px auto;
+    font-family: 'Roboto Condensed', sans-serif;
+    border-radius: 10px;
+}
+h1 {
+    background:#f2f2f2;
+    color:#5c8578;
+    padding:10px;
+    font-size:24px;
+    text-transform: uppercase;
+    text-align: center;
+    margin-bottom: 0px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+}
+ul {
+    list-style-type: none;
+    margin:0px;
+    padding:0px;
+}
+li {
+    font-size: 24px;
+    border-bottom:1px solid #f2f2f2;
+    padding:10px 20px;
+}
+li:last-child{
+    border:0px;
+}
+li.completed {
+    text-decoration: line-through;
+    color:darkgrey;
+}
+</style>
